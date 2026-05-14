fba5b97055c098406938394441e6c24c5c8fd93f
Md Nazir Hossain <nazirhossain.shaon@gmail.com>
2026-05-15 00:55:13 +0600

    fixed github login sync issue

--- index.js
+++ index.js
@@ -101,6 +101,7 @@ async function run() {
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
           })
+          
           .send({ success: true })
       } catch (err) {
         res.status(500).send(err)
